2cdf57a60184eccafc4be2354143111381434d9b
Anatolii <dinexpod@gmail.com>
2019-02-28 10:43:04 +0200

    - you don't have access to page "login" after logging

--- src/main/java/mate/academy/spring/config/SecurityConfig.java
+++ src/main/java/mate/academy/spring/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
 
         http.authorizeRequests()
                 .antMatchers("/resources/**", "/registration", "/login")
